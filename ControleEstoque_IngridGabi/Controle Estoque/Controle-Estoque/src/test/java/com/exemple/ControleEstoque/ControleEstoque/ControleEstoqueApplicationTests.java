77c613a8d161e024947fe629298944337eeb171a
GabrielleGoulart <gabriellegoulart48@gmail.com>
2025-05-20 16:49:10 -0300

    Add files via upload

---
+++ ControleEstoque_IngridGabi/Controle Estoque/Controle-Estoque/src/test/java/com/exemple/ControleEstoque/ControleEstoque/ControleEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemple.ControleEstoque.ControleEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
