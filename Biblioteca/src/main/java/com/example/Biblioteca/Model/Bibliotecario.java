3a869af1de1091a62038bf9a29ff01354cdfd327
GabrielleGoulart <gabriellegoulart48@gmail.com>
2025-06-15 19:41:40 -0300

    Add files via upload

---
+++ Biblioteca/src/main/java/com/example/Biblioteca/Model/Bibliotecario.java
@@ -0,0 +1,11 @@
+package com.example.Biblioteca;
+
+public class Bibliotecario {
+
+    private int idBibliotecario; // Corresponde a ID_Bibliotecario (camelCase é padrão em Java)
+    private String nome;         // Corresponde a Nome
+    private String email;        // Corresponde a Email
+
+    
+    public Bibliotecario() {
+    }
